9bd67cc78c6b3aac6e0973d5b0fcfa561e0021ef
“preetimca2016” <preetimca2016@gmail.com>
2022-09-30 16:27:46 +0530

    Server Connection Establish

---
+++ ADO.Net/ADO_AddressBook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO.Net
+{
+    public class ADO_AddressBook
+    {
+        /// <summary>
+        /// Establish Connection using connection string
+        /// </summary>
+        public static string ConnectionString = @"Data Source=(localdb)\ProjectModels;Initial Catalog=AddressBook_ServiceDB;Integrated Security=True";
+        public static void GetAllEmployees()
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(ConnectionString);
+                //open connection
+                connection.Open();
+                using (connection)
+                {
+                    Console.WriteLine("Successfully connection created");
+                }
+                //Close connection
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ADO.Net/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ADO_AddressBook.GetAllEmployees();
+        }
+    }
+}
